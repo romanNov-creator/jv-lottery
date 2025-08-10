5f0fb1c52c3ea4a7b788437da48c76b5be7bc682
Roman Novikov <novikdev12@gmail.com>
2025-08-10 13:17:41 +0200

    added private final ColorSuplaer and constant BALLS_TO_CREATE

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER_BOUND = 101;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER_BOUND));
     }
 }
